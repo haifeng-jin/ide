1d1575c1382918816bdbcca275f8fdc915a3c02b
Haifeng Jin <jhfjhfj1@gmail.com>
2021-03-29 14:05:46 -0500

    update

---
+++ base/run.sh
@@ -0,0 +1,8 @@
+docker run -it --rm \
+    -v ~/git:/home/me/workspace\
+    -e HOST_USER_ID=$(id -u $USER) \
+    -e HOST_GROUP_ID=$(id -g $USER) \
+    -e GIT_USER_NAME="Haifeng Jin" \
+    -e GIT_USER_EMAIL="haifeng-jin@users.noreply.github.com" \
+    -v ~/.ssh:/home/me/.ssh \
+    $1
