1419cae0deb8b1cf5ec887278cf4762b27079db2
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2021-08-06 17:25:30 -0700

    update

--- keras/run.sh
+++ keras/run.sh
@@ -1,3 +1,8 @@
 docker run -it --rm \
-    -v ~/git/keras:/home/workspace/keras\
+    -v ~/git/keras:/home/me/workspace/keras\
+    -e HOST_USER_ID=$(id -u $USER) \
+    -e HOST_GROUP_ID=$(id -g $USER) \
+    -e GIT_USER_NAME="Haifeng Jin" \
+    -e GIT_USER_EMAIL="haifeng-jin@users.noreply.github.com" \
+    -v ~/.ssh:/home/me/.ssh \
     haifengjin/ide-keras
